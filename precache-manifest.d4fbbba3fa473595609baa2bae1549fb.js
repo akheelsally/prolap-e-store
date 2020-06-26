30ab0e368b15a4748b1ec0cb6e3bf03111d6aa9b
akheelysally@gmail.com <akheelysally@gmail.com>
2020-06-26 12:05:12 +0500

    Updates

---
+++ precache-manifest.d4fbbba3fa473595609baa2bae1549fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7ea70a30823fd17244fb8a8103b3b1",
+    "url": "/prolap-e-store/index.html"
+  },
+  {
+    "revision": "c98fdd2554c39cc2a7af",
+    "url": "/prolap-e-store/static/css/main.fdae32e1.chunk.css"
+  },
+  {
+    "revision": "15376cf012a748d4be74",
+    "url": "/prolap-e-store/static/js/2.fd73a8ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prolap-e-store/static/js/2.fd73a8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98fdd2554c39cc2a7af",
+    "url": "/prolap-e-store/static/js/main.83560272.chunk.js"
+  },
+  {
+    "revision": "6b7478d5d79e19fc1982",
+    "url": "/prolap-e-store/static/js/runtime-main.baff84ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prolap-e-store/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "96c2a2735545da6fc30ff46a405d91a1",
+    "url": "/prolap-e-store/static/media/prolap-logo.96c2a273.jpg"
+  }
+]);
